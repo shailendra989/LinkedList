8dbc64e701e315aa1f139145d5a02fb21ce85a65
Shailendra kumar pal <palshailendrakumar123@gmail.com>
2023-01-30 23:09:58 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace DataStuctureDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 51);
+            list.RemoveFirstNode();
+            list.Search(51);
+            list.RemoveLastNode();
+            list.Display();
+
+            Console.WriteLine("Hello World!");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World!");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();// this.top=this.top.next
+            Console.ReadKey();
+        }
+    }
+}
